1629fde4c86cf3b5a8772297633ed03fe78515b3
Prasad Kumar <kprasadkumar7@gmail.com>
2022-03-31 20:43:07 +0530

    JS Template Strings

---
+++ 05_templateStrings.js
@@ -0,0 +1,19 @@
+// Template Strings in JavaScript
+
+let firstName = "Prasad";
+let lastName = "Kumar";
+let age = 24;
+
+// without template strings
+
+console.log("My name is " + firstName + " " + lastName + " and my age is " + age );   // My name is Prasad Kumar and my age is 24
+
+// with template strings
+
+console.log(`My name is ${firstName} ${lastName} and my age is ${age}`);              // My name is Prasad Kumar and my age is 24
+
+let a = 10;
+let b = 20;
+console.log(`sum of a,b is ${(a+b)}`);             // sum of a,b is 30
+
+
