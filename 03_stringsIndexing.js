602f6a9154477439bafe4a49ac90ee3db24f6124
Prasad Kumar <kprasadkumar7@gmail.com>
2022-03-31 20:26:32 +0530

    JS String I ndexing

---
+++ 03_stringsIndexing.js
@@ -0,0 +1,12 @@
+// String Indexing in JavaScript
+
+let myFullName = "Prasad Kumar";
+
+// P r a s a d   K u m a  r
+// 0 1 2 3 4 5 6 7 8 9 10 11 
+
+console.log(myFullName[0]);                         // it will print "P"
+console.log(myFullName[-1]);                        // it will print undefined
+console.log(myFullName[myFullName.length-1]);       // it will print "r"
+console.log(myFullName[5]);                         // it will print "d"
+console.log(myFullName.length);                     // it will print 12
