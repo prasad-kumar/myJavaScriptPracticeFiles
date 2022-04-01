ec9e3da469a554ff7f9d9528be359f83033d3894
Prasad Kumar <kprasadkumar7@gmail.com>
2022-04-01 17:54:43 +0530

    JS For Loops

--- 09_forLoops.js
+++ 09_forLoops.js
@@ -22,3 +22,45 @@ for (let i of fruits){
 };
 
 
+// nested for of loop
+
+const fruits2 = ["Mango", "Grapes", "Apple", "Orange" ];
+
+for (let j of fruits2){
+    console.log(j);
+    for (let k of j){
+        console.log(" |_", k);
+    };
+};
+
+// output:-
+
+// Mango
+//  |_ M
+//  |_ a
+//  |_ n
+//  |_ g
+//  |_ o
+// Grapes
+//  |_ G
+//  |_ r
+//  |_ a
+//  |_ p
+//  |_ e
+//  |_ s
+// Apple
+//  |_ A
+//  |_ p
+//  |_ p
+//  |_ l
+//  |_ e
+// Orange
+//  |_ O
+//  |_ r
+//  |_ a
+//  |_ n
+//  |_ g
+//  |_ e
+
+
+
