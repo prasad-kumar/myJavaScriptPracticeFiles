53e25f00a8a548bfb6bfb3b226126a89b6d0a6b9
Prasad Kumar <kprasadkumar7@gmail.com>
2022-04-01 17:42:38 +0530

    JS Array Destructuring

---
+++ 13_arrayDestructuring.js
@@ -0,0 +1,21 @@
+// Array Destructuring in JavaScript
+
+const fruits = ["Apple", "Banana", "Mango", "Orange"];
+
+let [item1, item2, item3, item4] = fruits;
+
+console.log(item1);           // Apple
+console.log(item2);           // Banana
+console.log(item4);           // Orange
+
+
+const myNumbers = [1, 2, 3, 4, 5];
+
+let [one, , ,four , ] = myNumbers;
+
+console.log(one);            // 1
+console.log(four);           // 4
+
+
+
+
