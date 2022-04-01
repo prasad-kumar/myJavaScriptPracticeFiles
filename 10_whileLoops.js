38ac9d94041c904348f282e632a637570947cff0
Prasad Kumar <kprasadkumar7@gmail.com>
2022-04-01 17:01:16 +0530

    JS While Loop

---
+++ 10_whileLoops.js
@@ -0,0 +1,23 @@
+// While and Do While Loops in JavaScript
+
+let i = 0;
+
+// print 1-10 numbers using while loop
+
+while (i <= 10){
+    console.log(i);
+    i++;
+};
+
+
+// print 1-10 numbers using do while loop
+
+let j = 1;
+do{
+    console.log(j);
+    j++;
+}
+while(j <= 10);
+
+
+
