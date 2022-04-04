68fcaa55f597a662168ef7c5f6afa72b85902f9a
Prasad Kumar <kprasadkumar7@gmail.com>
2022-04-04 20:47:33 +0530

    JS Closures

---
+++ 22_closures.js
@@ -0,0 +1,31 @@
+// Closures in JavaScript
+
+
+const myFunc = () => {
+    let count = 0
+    return () => {
+        if (count === 0){
+            console.log(`Hi`);
+            count += 1
+        }else{
+            console.log(`you've called me before ${count} times`)
+            count += 1
+        }
+    }
+}
+
+const fun = myFunc();  // myFunc will return a function with local variables
+fun();                 // fun() can access myFunc() local variables
+fun();
+fun();
+
+
+const powerOf = (power) => {
+    return (number) => {
+        return number ** power
+    }
+}
+
+const result = powerOf(2);
+
+console.log(result(2));
